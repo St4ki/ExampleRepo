4de0e92a04da6cf9c575b2d31380c4a72191de21
St4ki <tdennis5470@stu.neisd.net>
2023-09-05 11:06:57 -0500

    Removed Password

--- ExamplePython.py
+++ ExamplePython.py
@@ -1,7 +1,3 @@
-deff test_password(x):
-	print(x)
-
-
 #This is how we learned to concatenate strings in the ITF+ class.
 a = "String 1"
 b = "String 2"
@@ -19,6 +15,3 @@ job_list = {'John': 'Doctor', 'Jane': 'Engineer', 'Jim': 'Teacher'}
 
 for name, job in job_list.items():
     print(f"{name} is a {job}")
-
-password = "SuperSecretAdminPass"
-test_password(Password)
